fe2e38108c6ba3487d883c638c90c1d2d3cf1537
Anders Riutta <git@andersriutta.com>
2021-01-29 19:02:28 -0800

    prev. commit worked on mybinder. trying jupyterWith now.

--- default.nix
+++ default.nix
@@ -1,20 +1,27 @@
-with import <nixpkgs> {};
 let
-  pythonEnv = python38.withPackages (ps: [
-    ps.numpy
-    ps.toolz
-    ps.numpy
-    ps.scipy
-    ps.jupyterlab
-  ]);
-in mkShell {
-  buildInputs = [
-    pythonEnv
+  # Path to the JupyterWith folder.
+  jupyterWithPath = builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    rev = "35eb565c6d00f3c61ef5e74e7e41870cfa3926f7";
+  };
 
-    black
-    mypy
-
-    libffi
-    openssl
+  # Importing overlays from that path.
+  overlays = [
+    # Only necessary for Haskell kernel
+    (import "${jupyterWithPath}/nix/haskell-overlay.nix")
+    # Necessary for Jupyter
+    (import "${jupyterWithPath}/nix/python-overlay.nix")
+    (import "${jupyterWithPath}/nix/overlay.nix")
   ];
-}
+
+  # Your Nixpkgs snapshot, with JupyterWith packages.
+  pkgs = import <nixpkgs> { inherit overlays; };
+
+  # From here, everything happens as in other examples.
+  jupyter = pkgs.jupyterWith;
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+    };
+in
+  jupyterEnvironment.env
