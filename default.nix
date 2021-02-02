6f7220140e436c42d843c1dd6cd367d056365f87
Anders Riutta <git@andersriutta.com>
2021-02-02 09:31:10 -0800

    Add simplegeneric, ensure SSL_CERT_FILE and update pinned nixpkgs.

--- default.nix
+++ default.nix
@@ -99,6 +99,8 @@ let
       #google_cloud_automl
       #google_cloud_storage
 
+      # some of these may be needed to make rpy2 work
+      simplegeneric
       # tzlocal is needed to make rpy2 work
       tzlocal
       rpy2
@@ -167,5 +169,26 @@ in
   jupyterEnvironment.env.overrideAttrs (oldAttrs: {
     shellHook = oldAttrs.shellHook + ''
     . "${mynixpkgs.jupyterlab-connect}"/share/bash-completion/completions/jupyterlab-connect.bash
+    # this is needed in order that tools like curl and git can work with SSL
+    if [ ! -f "$SSL_CERT_FILE" ] || [ ! -f "$NIX_SSL_CERT_FILE" ]; then
+      candidate_ssl_cert_file=""
+      if [ -f "$SSL_CERT_FILE" ]; then
+        candidate_ssl_cert_file="$SSL_CERT_FILE"
+      elif [ -f "$NIX_SSL_CERT_FILE" ]; then
+        candidate_ssl_cert_file="$NIX_SSL_CERT_FILE"
+      else
+        candidate_ssl_cert_file="/etc/ssl/certs/ca-bundle.crt"
+      fi
+      if [ -f "$candidate_ssl_cert_file" ]; then
+          export SSL_CERT_FILE="$candidate_ssl_cert_file"
+          export NIX_SSL_CERT_FILE="$candidate_ssl_cert_file"
+      else
+        echo "Cannot find a valid SSL certificate file. curl will not work." 1>&2
+      fi
+    fi
+    #export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+
+    # set SOURCE_DATE_EPOCH so that we can use python wheels
+    SOURCE_DATE_EPOCH=$(date +%s)
     '';
   })
